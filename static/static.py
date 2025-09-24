3ff4a956cde00d68c71d5f723674fa331a404f94
kenx54 <speed70x@gmail.com>
2025-09-24 06:13:46 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,10 +1,6 @@
 # static.py
 
 HTML = """
-A ninety-year-old man finished the Athens marathon last year.
-If she wiggled her nose around she could make herself sneeze at will.
-Here's my big brother. Doesn't he look good?
-"""
 <!DOCTYPE html>
 <html>
 <head>
